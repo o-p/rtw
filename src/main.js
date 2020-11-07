30cc26df61807372fcd35d3f21f13b16ec4768e9
Chris Chu <op.github.io@gmail.com>
2020-11-07 22:46:38 +0800

    Import BootstrapVue and build-in icons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
+
 import App from './App.vue';
 import store from './store';
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 Vue.config.productionTip = false;
 
